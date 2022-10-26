df4040ac99734b122ff66817611c158171b27993
Niklas May <post@niklas-may.de>
2022-10-26 13:19:23 +0200

    initial setup

---
+++ schemas/index.ts
@@ -0,0 +1,63 @@
+
+
+const supportedLanguages = [
+    {
+      code: "en",
+      iso: "en-US",
+      name: "English",
+      file: "en.js",
+      isDefault: true,
+    },
+    {
+      code: "es",
+      iso: "es-ES",
+      name: "Spanish",
+      file: "es.js",
+    },
+  ]
+
+
+export const schemaTypes = [
+  {
+    name: 'test',
+    type: 'document',
+    fields: [
+      {
+        name: 'sections',
+        type: 'array',
+        of: [ {
+            name: "sectionA",
+            type: "object",
+            groups: [{name: "a"}, {name: "b"}],
+            fields: [
+                {
+                    name: 'title',
+                    type: 'object',
+                    group: 'a',
+                    fieldsets: [
+                      {
+                        title: 'Translations',
+                        name: 'translations',
+                        options: { collapsible: true, collapsed: false },
+                      },
+                    ],
+                    fields: supportedLanguages.map((lang) => ({
+                      title: lang.name,
+                      name: lang.code,
+                      type: 'string',
+                      fieldset: lang.isDefault ? null : 'translations',
+                    })),
+                },
+                {
+                    name: 'lorem',
+                    type: 'string',
+                    group: 'b',
+                }
+
+            ]
+        },
+        ],
+      },
+    ],
+  },
+]
